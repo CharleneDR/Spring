62361c1106443c5915fd822fb6e8620b47f5bd62
CharleneDR <cdarugna@sqli.com>
2023-03-28 21:16:25 +0200

    Quête JDBC 01

---
+++ JDBC_01/src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -0,0 +1,116 @@
+package com.wildcodeschool.wildandwizard.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.util.JdbcUtils;
+
+public class SchoolRepository {
+
+    private final static String DB_URL = "jdbc:postgresql://localhost:5432/spring_jdbc_quest";
+    private final static String DB_USER = "wilder";
+    private final static String DB_PASSWORD = "Hellothere.03";;
+
+    public List<School> findAll() {
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD);
+            statement = connection.prepareStatement(
+                    "SELECT * FROM school;");
+            resultSet = statement.executeQuery();
+
+            List<School> schools = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                String country = resultSet.getString("country");
+                schools.add(new School(id, name, capacity, country));
+            }
+            return schools;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResultSet(resultSet);
+            JdbcUtils.closeStatement(statement);
+            JdbcUtils.closeConnection(connection);
+        }
+
+        return null;
+    }
+
+    public School findById(Long id) {
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD);
+            statement = connection.prepareStatement(
+                    "SELECT * FROM school WHERE id = ?;");
+            statement.setLong(1, id);
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                String country = resultSet.getString("country");
+                return new School(id, name, capacity, country);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResultSet(resultSet);
+            JdbcUtils.closeStatement(statement);
+            JdbcUtils.closeConnection(connection);
+        }
+        return null;
+    }
+
+    public List<School> findByCountry(String countrySearched) {
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD);
+            statement = connection.prepareStatement(
+                    "SELECT * FROM school WHERE country= ?;");
+            statement.setString(1, countrySearched);
+            resultSet = statement.executeQuery();
+
+            List<School> schools = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                String country = resultSet.getString("country");
+                schools.add(new School(id, name, capacity, country));
+            }
+            return schools;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResultSet(resultSet);
+            JdbcUtils.closeStatement(statement);
+            JdbcUtils.closeConnection(connection);
+        }
+
+        return null;
+    }
+}
