5f9aba5c18bed63aad66364c033b93f945633032
CharleneDR <cdarugna@sqli.com>
2023-03-30 17:10:54 +0200

    Quête JPA 2

---
+++ JPA_2/src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -0,0 +1,68 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.*;
+
+import java.sql.Date;
+
+import java.util.*;
+
+@Entity
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long capacity;
+    private String country;
+
+    @OneToMany(mappedBy = "school")
+    private List<Wizard> wizards;
+
+    public School() {
+    }
+
+    public List<Wizard> getWizards() {
+        return this.wizards;
+    }
+
+    public void setWizards(List<Wizard> wizards) {
+        this.wizards = wizards;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Long capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
